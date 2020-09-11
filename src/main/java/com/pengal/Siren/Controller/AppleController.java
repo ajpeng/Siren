968717453395670daf26c3ffd8d73052c4a13299
craexus <jjpeng@ryerson.ca>
2020-09-11 07:06:41 -0400

    Added error handler

--- src/main/java/com/pengal/Siren/Controller/AppleController.java
+++ src/main/java/com/pengal/Siren/Controller/AppleController.java
@@ -5,15 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class AppleController {
 
     @Autowired AppleRepository appleRepository;
 
     @GetMapping("/top100")
     public String top100() {
-        return "TODO";
+        return "{\"TODO\"}";
     }
 
     @GetMapping("/search")
--- src/main/java/com/pengal/Siren/Controller/CommonController.java
+++ src/main/java/com/pengal/Siren/Controller/CommonController.java
@@ -6,8 +6,4 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CommonController {
 
-    @RequestMapping("/error")
-    public String handleError() {
-        return "Oops looks like something went wrong";
-    }
 }
