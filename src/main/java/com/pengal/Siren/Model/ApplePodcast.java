0d2b26dbab0835105e4ed45eb37389836e9b34e6
craexus <jjpeng@ryerson.ca>
2020-10-05 12:20:50 -0400

    Apple JPA mapping now working

--- src/main/java/com/pengal/Siren/Model/ApplePodcast.java
+++ src/main/java/com/pengal/Siren/Model/ApplePodcast.java
@@ -1,17 +1,273 @@
 package com.pengal.Siren.Model;
 
 import javax.persistence.*;
+import java.util.LinkedList;
+import java.util.List;
 
 @Entity
 @Table(name = "podcasts")
 public class ApplePodcast {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String artistName;
+    private String collectionName;
+    private String trackName;
+    private String country;
+    private String primaryGenreName;
+    private String artworkUrl;
+    private String wrapperType;
+    private String kind;
+    private String collectionCensoredName;
+    private String trackCensoredName;
+    private String artistViewUrl;
+    private String collectionViewUrl;
+    private String feedUrl;
+    private String trackViewUrl;
+    private String artworkUrl30;
+    private String artworkUrl60;
+    private String artworkUrl100;
+    private String currency;
+    private String contentAdvisoryRating;
+    private String artworkUrl600;
+    private String collectionExplicitness;
+    private String trackExplicitness;
+    private String releaseDate;
 
-    private Long artistId;
-    private Long collectionId;
-    private Long trackId;
+    private long id;
+    private long artistId;
+    private long collectionId;
+    private long trackId;
+    private long trackRentalPrice;
+    private long collectionHdPrice;
+    private long trackHdPrice;
+    private long trackHdRentalPrice;
+    private long trackCount;
+
+    private double collectionPrice;
+    private double trackPrice;
+    @Column
+    @ElementCollection(targetClass=Integer.class)
+    private List<String> genreIds;
+    @Column
+    @ElementCollection(targetClass=Integer.class)
+    private List<String> genres;
+
+    public String getWrapperType() {
+        return wrapperType;
+    }
+
+    public void setWrapperType(String wrapperType) {
+        this.wrapperType = wrapperType;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public String getCollectionCensoredName() {
+        return collectionCensoredName;
+    }
+
+    public void setCollectionCensoredName(String collectionCensoredName) {
+        this.collectionCensoredName = collectionCensoredName;
+    }
+
+    public String getTrackCensoredName() {
+        return trackCensoredName;
+    }
+
+    public void setTrackCensoredName(String trackCensoredName) {
+        this.trackCensoredName = trackCensoredName;
+    }
+
+    public String getArtistViewUrl() {
+        return artistViewUrl;
+    }
+
+    public void setArtistViewUrl(String artistViewUrl) {
+        this.artistViewUrl = artistViewUrl;
+    }
+
+    public String getCollectionViewUrl() {
+        return collectionViewUrl;
+    }
+
+    public void setCollectionViewUrl(String collectionViewUrl) {
+        this.collectionViewUrl = collectionViewUrl;
+    }
+
+    public String getFeedUrl() {
+        return feedUrl;
+    }
+
+    public void setFeedUrl(String feedUrl) {
+        this.feedUrl = feedUrl;
+    }
+
+    public String getTrackViewUrl() {
+        return trackViewUrl;
+    }
+
+    public void setTrackViewUrl(String trackViewUrl) {
+        this.trackViewUrl = trackViewUrl;
+    }
+
+    public String getArtworkUrl30() {
+        return artworkUrl30;
+    }
+
+    public void setArtworkUrl30(String artworkUrl30) {
+        this.artworkUrl30 = artworkUrl30;
+    }
+
+    public String getArtworkUrl60() {
+        return artworkUrl60;
+    }
+
+    public void setArtworkUrl60(String artworkUrl60) {
+        this.artworkUrl60 = artworkUrl60;
+    }
+
+    public String getArtworkUrl100() {
+        return artworkUrl100;
+    }
+
+    public void setArtworkUrl100(String artworkUrl100) {
+        this.artworkUrl100 = artworkUrl100;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getContentAdvisoryRating() {
+        return contentAdvisoryRating;
+    }
+
+    public void setContentAdvisoryRating(String contentAdvisoryRating) {
+        this.contentAdvisoryRating = contentAdvisoryRating;
+    }
+
+    public String getArtworkUrl600() {
+        return artworkUrl600;
+    }
+
+    public void setArtworkUrl600(String artworkUrl600) {
+        this.artworkUrl600 = artworkUrl600;
+    }
+
+    public String getCollectionExplicitness() {
+        return collectionExplicitness;
+    }
+
+    public void setCollectionExplicitness(String collectionExplicitness) {
+        this.collectionExplicitness = collectionExplicitness;
+    }
+
+    public String getTrackExplicitness() {
+        return trackExplicitness;
+    }
+
+    public void setTrackExplicitness(String trackExplicitness) {
+        this.trackExplicitness = trackExplicitness;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setArtistId(long artistId) {
+        this.artistId = artistId;
+    }
+
+    public void setCollectionId(long collectionId) {
+        this.collectionId = collectionId;
+    }
+
+    public void setTrackId(long trackId) {
+        this.trackId = trackId;
+    }
+
+    public long getTrackRentalPrice() {
+        return trackRentalPrice;
+    }
+
+    public void setTrackRentalPrice(long trackRentalPrice) {
+        this.trackRentalPrice = trackRentalPrice;
+    }
+
+    public long getCollectionHdPrice() {
+        return collectionHdPrice;
+    }
+
+    public void setCollectionHdPrice(long collectionHdPrice) {
+        this.collectionHdPrice = collectionHdPrice;
+    }
+
+    public long getTrackHdPrice() {
+        return trackHdPrice;
+    }
+
+    public void setTrackHdPrice(long trackHdPrice) {
+        this.trackHdPrice = trackHdPrice;
+    }
+
+    public long getTrackHdRentalPrice() {
+        return trackHdRentalPrice;
+    }
+
+    public void setTrackHdRentalPrice(long trackHdRentalPrice) {
+        this.trackHdRentalPrice = trackHdRentalPrice;
+    }
+
+    public long getTrackCount() {
+        return trackCount;
+    }
+
+    public void setTrackCount(long trackCount) {
+        this.trackCount = trackCount;
+    }
+
+    public double getCollectionPrice() {
+        return collectionPrice;
+    }
+
+    public void setCollectionPrice(double collectionPrice) {
+        this.collectionPrice = collectionPrice;
+    }
+
+    public double getTrackPrice() {
+        return trackPrice;
+    }
+
+    public void setTrackPrice(double trackPrice) {
+        this.trackPrice = trackPrice;
+    }
+
+    public List<String> getGenreIds() {
+        return genreIds;
+    }
+
+    public void setGenreIds(List<String> genreIds) {
+        this.genreIds = genreIds;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<String> genres) {
+        this.genres = genres;
+    }
+
 
     public Long getId() {
         return id;
@@ -100,13 +356,4 @@ public class ApplePodcast {
     public void setArtworkUrl(String artworkUrl) {
         this.artworkUrl = artworkUrl;
     }
-
-    private String artistName;
-    private String collectionName;
-    private String trackName;
-
-    private String releaseDate;
-    private String country;
-    private String primaryGenreName;
-    private String artworkUrl;
 }
